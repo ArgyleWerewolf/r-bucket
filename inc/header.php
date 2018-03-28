98fa33436dec942114ece36f67bafcffaacf5c76
A. Quinton <argylewerewolf@gmail.com>
2018-03-27 17:47:09 -0700

    Foundation CSS is loaded from a local file instead of a CDN

--- inc/header.php
+++ inc/header.php
@@ -1,8 +1,8 @@
 <html>
 <head>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.1/css/foundation.min.css" />
+  <link rel="stylesheet" href="css/foundation-v6.4.1.min.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
-  <link rel="stylesheet" href="styles.css" />
+  <link rel="stylesheet" href="css/styles.css" />
   <title><?php echo SITE_TITLE; ?>!!</title>
 </head>
 <body>
